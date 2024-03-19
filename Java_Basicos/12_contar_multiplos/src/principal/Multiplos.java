a8ab956e62d5446e682c1754093addaa1ee92f58
Daniel Guerrero Raez <guerreroraezdaniel@hotmail.com>
2024-03-19 13:38:36 +0100

    --

--- Java_Basicos/12_contar_multiplos/src/principal/Multiplos.java
+++ Java_Basicos/12_contar_multiplos/src/principal/Multiplos.java
@@ -9,20 +9,30 @@ public class Multiplos {
 	   //Y nos diga cuantos multiplos de 5 hay entre ambos numeros
 		int num1;
 		int num2;
+		int numMenor;
+		int numMayor;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Introduce el primer número:");
 		num1=sc.nextInt();
 		System.out.println("Introduce el segundo número:");
 		num2=sc.nextInt();
 		
+		if(num1<num2) {
+			numMenor=num1;
+			numMayor=num2;
+		}else {
+			numMenor=num2;
+			numMayor=num1;
+		}
+		
         int contador = 0;
 
-        for (int i = num1; i <= num2; i++) {
+        for (int i = numMenor; i <= numMayor; i++) {
             if (i % 5 == 0) {
                 contador++; 
             }
         }
-        System.out.println("Entre el número " +num1+ " y " +num2+ " hay " +contador+ "multiplos de 5");
+        System.out.println("Entre el número " +num1+ " y " +num2+ " hay " +contador+ " multiplos de 5");
 
        
     }
