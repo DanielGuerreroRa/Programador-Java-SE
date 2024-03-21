5d3a1cbfe2754bde60f4e72bb19886269fa26f81
Daniel Guerrero Raez <guerreroraezdaniel@hotmail.com>
2024-03-21 11:30:05 +0100

    --

--- Java_Basicos/20_prueba_string/src/principal/PruebaTexto.java
+++ Java_Basicos/20_prueba_string/src/principal/PruebaTexto.java
@@ -4,8 +4,17 @@ public class PruebaTexto {
 
 	public static void main(String[] args) {
 		String n="hello";
-		n=n.toUpperCase();
+		n=n.toUpperCase(); //MAYUSCULAS
 		System.out.println(n);
+		
+		
+		String a = new String("miTexto");
+		String b = new String("miTexto");
+		if(a.equals(b)) { // 'equals' compara el objeto a con el objeto b 
+			System.out.println("Son iguales");    //equalsIgnoreCase() no hace distincion entre mayusculas y misnusculas
+		}else {
+			System.out.println("Son diferentes");
+		}
 
 	}
 
