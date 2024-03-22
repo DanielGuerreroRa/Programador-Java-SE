cdced17a8050bfbbf7be900ba6724393c7983078
Daniel Guerrero Raez <guerreroraezdaniel@hotmail.com>
2024-03-22 09:44:13 +0100

    --

--- Java_Basicos/24_uso_split/src/principal/BuscarNombre.java
+++ Java_Basicos/24_uso_split/src/principal/BuscarNombre.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class BuscarNombre {
 	public static void main(String[] args) {
 		
-		String cesta ="tomates,patatas,vino,leche,azucar,vino,pan,vino,agua,limon.brocoli,salchichas,pan";
+		String cesta ="tomates,patatas,vino,leche,azucar,vino,pan,vino,agua,limon,brocoli,salchichas,pan";
 		
 		//Se pide la introducción d eun producto, y el programa nos muestra su stock
 		
 		Scanner teclado = new Scanner(System.in);
 		String[] productos = cesta.split(","); //Esto nos devolvera una array con todos los productos separados por ","
-		String producto;
+		String producto;   //Tambien podemos poner cesta.split("[ ,.]"); pata poner varios caracteres o espacios
 		int stock=0;
 		System.out.println("Introduce el producto: ");
 		producto=teclado.nextLine(); //le pedimos el producto
--- Java_Basicos/25_direcciones_email/src/principal/ComprobarDirecciones.java
+++ Java_Basicos/25_direcciones_email/src/principal/ComprobarDirecciones.java
@@ -3,8 +3,21 @@ package principal;
 public class ComprobarDirecciones {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String emails="aaa@gmail.com,bbb@tel.es,ccc@gmail.es,ddd@gmail.com,abc@hotmail.com";
+		//indicar cuantas direcciones hay de cada dominio
+		int contEs=0,contCom=0;
+		String[] direcciones=emails.split(",");
+		for(String dir:direcciones) {
+			switch(dir.substring(dir.indexOf(".")+1,dir.length())) {
+				case "es":
+					contEs++;
+					break;
+				case "com":
+					contCom++;
+			}
+		}
+		System.out.println("Direcciones de correo con dominio .es: "+contEs);
+		System.out.println("Direcciones de correo con dominio .com: "+contCom);
 	}
 
 }
