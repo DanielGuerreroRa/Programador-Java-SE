4f5ae8f7752f017136c58bbb394421f2d599eb5c
Daniel Guerrero Raez <guerreroraezdaniel@hotmail.com>
2024-04-03 09:28:56 +0200

    --

--- Java_Basicos/34_notas_capas/src/service/NotasService.java
+++ Java_Basicos/34_notas_capas/src/service/NotasService.java
@@ -1,5 +1,38 @@
 package service;
 
+import java.util.ArrayList;
+
 public class NotasService {
+
+	//	Realizar una nueva versión del programa de las notas. Al iniciar el programa
+	//	aparecerá el siguiente menú:
+	//	1.- Nueva nota
+	//	2.- Calcular media
+	//	3.- Notas extremas
+	//	4.- Ver todas
+	//	5.- Salir 
+
+	ArrayList<Double> notas=new ArrayList<Double>();
+
+	public void agregarNota(double nota) {
+      nota++;
+	}
+
+	public double media() {
+      
+	}
+
+	public double max() {
+
+	}
+
+	public double min() {
+
+	}
+	
+	public double[] obtenernotas() {
+		
+	}
+
 
 }
