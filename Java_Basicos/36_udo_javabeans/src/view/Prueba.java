b458a69b9cc61b09e2c2d8d8e25cbd0edc764266
Daniel Guerrero Raez <guerreroraezdaniel@hotmail.com>
2024-04-03 13:58:04 +0200

    --

--- Java_Basicos/36_udo_javabeans/src/view/Prueba.java
+++ Java_Basicos/36_udo_javabeans/src/view/Prueba.java
@@ -1,9 +1,15 @@
 package view;
 
+import java.util.ArrayList;
+
+import model.Ciudad;
+
 public class Prueba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Ciudad ciudad = new Ciudad("Madrid",300000,23.05);
+		ArrayList<Ciudad> ciudades = new ArrayList<Ciudad>();
+		ciudades.add(ciudad);
 
 	}
 
