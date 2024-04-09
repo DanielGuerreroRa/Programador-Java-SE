70ee2ec95c297dad6449fd930975c76d27654666
Daniel Guerrero Raez <guerreroraezdaniel@hotmail.com>
2024-04-09 10:33:38 +0200

    --

--- Java_Basicos/45_curso_fichero/src/view/CursoFichero.java
+++ Java_Basicos/45_curso_fichero/src/view/CursoFichero.java
@@ -14,40 +14,39 @@ public class CursoFichero {
 
 	public static void main(String[] args) {
 		String [] cursos= {
-				"Java básico",
-				"Python para principaiantes",
-				"Java EE en Web",
-				"JavaScript en Front",
-				"POO con Java",
-				"Python y Django",
-				"Desarrollo Web con ASP.NET",
-				"Aplicaciones Web con Python"
-		}; 
+			"Java básico",
+			"Python para principaiantes",
+			"Java EE en Web",
+			"JavaScript en Front",
+			"POO con Java",
+			"Python y Django",
+			"Desarrollo Web con ASP.NET",
+			"Aplicaciones Web con Python"
+		};
+		
 		//Solicitamos por teclado la introducción de un nombre y guardaremos en un fichero todos los cursos que contengan ese nombre
-		Scanner teclado= new Scanner(System.in);
-		System.out.println("Introduce el nombre de un curso:");
-		String nombreCurso = teclado.nextLine();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Introduce nombre");
+		String nombre=sc.nextLine();
+		guardarCursos(nombre,cursos);
+	}
+	static void guardarCursos(String nombre,String[] cursos) {
 		String ruta="cursos.txt";
-		boolean cursoEncontrado = false;
-		try (FileOutputStream fos = new FileOutputStream(ruta, true);
-                PrintStream out = new PrintStream(fos);) {
-            for (String curso: cursos) {
-                if (curso.contains(nombreCurso)) {
-                    out.println(curso);
-                    cursoEncontrado = true;
-                }
-            }
+		try(PrintStream out=new PrintStream(ruta)) {
+			for(String c:cursos) {
+				//Si el curso contiene ese nombre se guarda
+				if(c.contains(nombre)) {
+					out.println(c);
+				}
+			}
+		}
+		catch(FileNotFoundException ex) {
+			ex.printStackTrace();
+		}
+		catch(IOException ex) {
+			ex.printStackTrace();
+		}
 
-            if (cursoEncontrado) {
-                System.out.println("Curso registrado");
-            } else {
-                System.out.println("Este curso no se encuentra en el fichero");
-            }
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+	}
 
-    }
 }
