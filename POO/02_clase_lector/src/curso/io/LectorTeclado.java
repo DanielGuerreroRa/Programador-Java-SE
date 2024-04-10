3b6bb9e4db3dc65699aff97986c8aec8a7f2a422
Daniel Guerrero Raez <guerreroraezdaniel@hotmail.com>
2024-04-10 13:38:01 +0200

    --

--- POO/02_clase_lector/src/curso/io/LectorTeclado.java
+++ POO/02_clase_lector/src/curso/io/LectorTeclado.java
@@ -1,10 +1,46 @@
 package curso.io;
 
-public class LectorTeclado {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class LectorTeclado extends BufferedReader{
+	//Crear una clase para lectura de datos por teclado. Esa clase, tendrá tres métodos:
+	//readString()
+	//readInt()
+	//readDouble() 
+
+	public LectorTeclado() {   //Hereda todo lo que tiene BufferredReader
+		super(new InputStreamReader(System.in)); //Para que podamos leer en el teclado
 
 	}
 
+	public String readString() {
+		try {
+			return readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null; //Si falla la lectura entregamos null
+		}
+	}
+
+	public int readInt() {
+		try {
+			return Integer.parseInt(readLine());
+		} catch (IOException e) {
+			e.printStackTrace();
+			return 0; //Si falla la lectura entregamos 0
+		}
+	}
+
+	public double readDouble()  {
+		try {
+			return Double.parseDouble(readLine());
+		} catch (IOException e) {
+			e.printStackTrace();
+			return 0.0; //Si falla la lectura entregamos 0.0
+		}
+	}
+	
+	
 }
