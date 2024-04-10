3c3e07a4d3ea94f3f8099959514f0baa61115a26
Daniel Guerrero Raez <guerreroraezdaniel@hotmail.com>
2024-04-10 12:06:12 +0200

    --

---
+++ Java_Basicos/00_ejercicios/src/pruebas_examen/Ejemplos.java
@@ -0,0 +1,10 @@
+package pruebas_examen;
+
+public class Ejemplos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Java_Basicos/49_gestor_pedidos_fichero_lombok/src/model/Pedido.java
+++ Java_Basicos/49_gestor_pedidos_fichero_lombok/src/model/Pedido.java
@@ -3,13 +3,13 @@ package model;
 import java.time.LocalDate;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
 
 public class Pedido {
 	private String producto;
