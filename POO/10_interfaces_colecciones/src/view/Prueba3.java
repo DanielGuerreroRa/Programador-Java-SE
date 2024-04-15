292f5d851004ffffe2c916d6b680f95720e98dc8
Daniel Guerrero Raez <guerreroraezdaniel@hotmail.com>
2024-04-15 13:55:43 +0200

    --

--- POO/10_interfaces_colecciones/src/view/Prueba3.java
+++ POO/10_interfaces_colecciones/src/view/Prueba3.java
@@ -1,15 +1,27 @@
 package view;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Prueba3 {
 
 	public static void main(String[] args) {
-		//Realizar un método que recibe dos colecciones de entero y devuelve con los números contenidos en ambas colecciones
-
+		ArrayList<Integer> nums=new ArrayList<Integer>();
+		nums.add(11);nums.add(7);nums.add(12);nums.add(31);
+		HashSet<Integer> nums2=new HashSet<Integer>();
+		nums2.add(11);nums2.add(4);nums2.add(1);nums2.add(31);
+		//System.out.println(iguales(nums,nums2));
+		//recorremos resultado con un Iterator
+		Set<Integer> res=contenido(nums,nums2);
+		Iterator<Integer> it=res.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
 	}
+	//Realizar un método que recibe dos colecciones de entero y devuelve con los números contenidos en ambas colecciones
 	public static Set<Integer> contenido(Collection<Integer> col1,Collection<Integer> col2) {
 		 Set<Integer> contenidoComun=new HashSet<>();
 		 for (Integer c1:col1) {
