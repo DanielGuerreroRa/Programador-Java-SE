a9e8889832f2c02d59219b7be5402a25c0d1384e
Daniel Guerrero Raez <guerreroraezdaniel@hotmail.com>
2024-04-16 13:57:32 +0200

    --

--- POO/14_operaciones_datos/src/view/AppNumeros.java
+++ POO/14_operaciones_datos/src/view/AppNumeros.java
@@ -2,23 +2,32 @@ package view;
 
 import java.util.List;
 
+import service.Condicion;
 import service.OperacionesService;
 
+class Positivos implements Condicion{
+	@Override
+	public boolean test(Integer n) {
+		return n>0;
+	}
+}
+class Pares implements Condicion{
+	@Override
+	public boolean test(Integer n) {
+		return n%2==0;
+	}
+	
 public class AppNumeros {
 
 	public static void main(String[] args) {
 		List<Integer> numeros=List.of(3,-2,8,5,1,17,22,-9,6,12);
+		OperacionesService service=new OperacionesService();
+		
+		//imprimir, por una lado, la suma de los pares
 		
 		
-		OperacionesService servicio=new OperacionesService();
-		
-		//Imprimir, por un lado, la suma de los pares 
-//		int sumaPares=servicio.sumarPorCondicion(numeros%2==0);
-//		System.out.println(sumaPares);
-//		//Y por otro la suma de los negativos
-//		int sumaNegativos=servicio.sumarPorCondicion(numeros<0);
-//		System.out.println(sumaNegativos);
-
+		//y por otro la suma de los positivos
+		System.out.println("Suma positivos: "+service.sumarPorCondicion(numeros, new Positivos()));
 	}
 
 }
